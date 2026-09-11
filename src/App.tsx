1f32860f71a4bf620579962725bfca1589df7424
Filberrt <danielslkhv@gmail.com>
2026-09-11 16:14:10 +0500

    Refresh product catalog imagery

--- src/App.tsx
+++ src/App.tsx
@@ -92,13 +92,13 @@ const productCards = [
     productId: 'manganese-flux',
     title: 'Марганцовистый флюс',
     text: 'Для агломерации, производства окатышей, доменных и сталеплавильных процессов.',
-    image: 'product/manganese-flux-line-wide.png',
+    image: 'product/manganese-flux-catalog.png',
   },
   {
     productId: 'gypsum-stone',
     title: 'Гипсовый и ангидритовый камень',
     text: 'Для цемента, сухих смесей, гипсовых и других строительных материалов.',
-    image: 'product/gypsum-anhydrite-line-wide.png',
+    image: 'product/gypsum-anhydrite-catalog.png',
   },
 ]
 
